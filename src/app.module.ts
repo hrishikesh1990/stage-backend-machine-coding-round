6e143d004741f9120e259d6a79cad98f2d8026ed
Bonnie Simon <bonnie.developer@gmail.com>
2024-12-28 12:16:17 +0530

    Add seeds. Add applogger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 import { TvshowsModule } from './tvshows/tvshows.module';
+import { SeedModule } from './seed/seed.module';
+import { AppLoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://mongodb:27017/stagedb'),
     MoviesModule,
     TvshowsModule,
+    SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AppLoggerMiddleware).forRoutes('*');
+  }
+}
